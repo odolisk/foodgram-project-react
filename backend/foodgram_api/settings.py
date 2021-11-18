c9cdb475308f652185c7a7b18bf89c23b26875de
Дмитров Артем <odolisk@yandex.ru>
2021-11-18 10:33:25 +0300

    Attempt to up

--- backend/foodgram_api/settings.py
+++ backend/foodgram_api/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'temp_value')
 
 DEBUG = (os.getenv('DEBUG', 'False') == 'True')
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default='localhost').split(' ')
+ALLOWED_HOSTS = (os.getenv('ALLOWED_HOSTS')).split()
 
 INSTALLED_APPS = [
     'django.contrib.admin',
